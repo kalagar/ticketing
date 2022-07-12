40b1f31caf31a52841b9c1e642b8713f19354fe6
Mansour Kalagar <mansour@foobar.agency>
2022-07-12 20:20:17 +0430

    local dev

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,7 @@ const app = express();
 app.use(json());
 
 app.get('/api/users/current', (req, res) => {
-  res.status(200).send({
-    id: 1,
-  });
+  res.send('Hi there!');
 });
 
 app.listen(3000, () => {
